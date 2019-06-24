818210e6acb21823d89d285311c50b5b3e6740a5
p-ger <p-ger@PapGergely-Base>
2019-06-24 20:19:05 +0200

    Add Spring nature to cts-common

---
+++ cts-common/src/main/java/prv/pgergely/cts/common/ApplicationCommon.java
@@ -0,0 +1,12 @@
+package prv.pgergely.cts.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCommon {
+	
+    public static void main( String[] args){
+    	SpringApplication.run(ApplicationCommon.class, args);
+    }
+}
