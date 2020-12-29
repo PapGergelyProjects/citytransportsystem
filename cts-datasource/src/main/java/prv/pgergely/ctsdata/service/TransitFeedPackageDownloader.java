68800b1990c221972d9210a5287ee098f2c01615
PapGergely <PapGergely@PapGergely-Base>
2020-12-29 09:19:35 +0100

    Fixing http method issue

--- cts-datasource/src/main/java/prv/pgergely/ctsdata/service/TransitFeedPackageDownloader.java
+++ cts-datasource/src/main/java/prv/pgergely/ctsdata/service/TransitFeedPackageDownloader.java
@@ -3,6 +3,8 @@ package prv.pgergely.ctsdata.service;
 import java.net.URI;
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpEntity;
@@ -11,6 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import prv.pgergely.ctsdata.config.TemplateConfig;
@@ -25,6 +28,8 @@ public class TransitFeedPackageDownloader {
 	@Autowired
 	@Qualifier(TemplateConfig.DEFAULT_TEMPLATE)
 	private RestTemplate defTemplate;
+	
+	private Logger logger = LogManager.getLogger(TransitFeedPackageDownloader.class);
 	
 	public ResponseEntity<byte[]> getZipFile(String urlAddress){
 		HttpHeaders headers = new HttpHeaders();
@@ -43,7 +48,12 @@ public class TransitFeedPackageDownloader {
 		headers.set("Content-Encoding", "UTF-8");
 		headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36");
 		HttpEntity<String> entity = new HttpEntity<>(headers);
+		try {
+			return defTemplate.postForLocation(urlAddress, entity);
+		}catch(HttpClientErrorException e) {
+			logger.warn(e.getStatusCode()+" - "+e.getStatusText());
+		}
 		
-		return defTemplate.postForLocation(urlAddress, entity);
+		return null;
 	}
 }
