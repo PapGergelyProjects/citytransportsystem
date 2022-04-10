d158fadf1cb19c8acd6620dd43b51884dada1bfc
PapGergely <PapGergely@PapGergely-Base>
2022-04-10 13:19:40 +0200

    Add spring profiles, set dev to default

--- cts-datasource/src/main/java/prv/pgergely/ctsdata/config/DatasourceConfig.java
+++ cts-datasource/src/main/java/prv/pgergely/ctsdata/config/DatasourceConfig.java
@@ -6,13 +6,23 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class DatasourceConfig{
 	
 	@Bean
-	@ConfigurationProperties("spring.datasource-secondary")
-	public DataSource getDatasource() {
+	@Profile("dev")
+	@ConfigurationProperties("spring.datasource-devel")
+	public DataSource getDatasourceDev() {
+		DataSourceBuilder dsBuild = DataSourceBuilder.create();
+		return dsBuild.build();
+	}
+	
+	@Bean
+	@Profile("prod")
+	@ConfigurationProperties("spring.datasource-primary")
+	public DataSource getDatasourceProd() {
 		DataSourceBuilder dsBuild = DataSourceBuilder.create();
 		return dsBuild.build();
 	}
