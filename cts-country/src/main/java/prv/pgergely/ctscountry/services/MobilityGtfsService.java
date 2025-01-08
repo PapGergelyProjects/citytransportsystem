9f5f57bd990f9a0be84c3902c86b9a5be1bdfdd3
PapGergely <PapGergely@PapGergely-Base>
2025-01-08 17:26:17 +0100

    Wiring mobility api WiP

--- cts-country/src/main/java/prv/pgergely/ctscountry/services/MobilityGtfsService.java
+++ cts-country/src/main/java/prv/pgergely/ctscountry/services/MobilityGtfsService.java
@@ -1,7 +1,6 @@
 package prv.pgergely.ctscountry.services;
 
 import java.time.OffsetDateTime;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import prv.pgergely.cts.common.domain.mobility.BoundingCoordinates;
 import prv.pgergely.cts.common.domain.mobility.GtfsFeedData;
-import prv.pgergely.cts.common.domain.transitfeed.FeedLocationsJson;
 import prv.pgergely.ctscountry.domain.mobility.feeds.ExternalIds;
 import prv.pgergely.ctscountry.domain.mobility.gtfs.BoundingBox;
 import prv.pgergely.ctscountry.domain.mobility.gtfs.Location;
@@ -27,10 +25,10 @@ public class MobilityGtfsService {
 	@Autowired
 	private FeedVersionServiceImpl feedVersion;
 	
-	public List<FeedLocationsJson> getFeedsByCountry(String country) {
+	public List<GtfsFeedData> getFeedsByCountry(String country) {
 		List<MobilityGtfsFeed> feeds = api.getAllGtfsFeeds(country);
 		Map<Long, FeedVersion> allRegisteredVersion = feedVersion.getFeedVersions().stream().collect(Collectors.toMap(k -> k.getFeedId(), v -> v));
-		feeds.stream().map(feed -> {
+		return feeds.stream().map(feed -> {
 			final ExternalIds extIds = feed.getExternalId().getFirst();
 			final Long externalId = Long.valueOf(extIds.getExternalId());
 			final FeedVersion vers = allRegisteredVersion.get(externalId);
@@ -40,8 +38,6 @@ public class MobilityGtfsService {
 			json.setTitle(feed.getProvider());
 			json.setDsUrl(vers.getSourceUrl());
 			json.setBoundingCoord(new BoundingCoordinates(bb.getMinLat(), bb.getMinLng(), bb.getMaxLat(), bb.getMaxLng()));
-			//json.setLat(loc.lat);
-			//json.setLon(loc.lng);
 			json.setEnabled(allRegisteredVersion.containsKey(json.getId()));
 			json.setActive(vers.isActive());
 			json.setSchemaName(vers.getSchemaName());
@@ -50,9 +46,7 @@ public class MobilityGtfsService {
 			json.setLatestVersion(OffsetDateTime.parse(feed.getLatestData().getDownloadAt()));
 			
 			return json;
-		});
-		
-		return Arrays.asList();
+		}).collect(Collectors.toList());
 	}
 	
 	private String refineLocationData(List<Location> locations) {
--- cts-country/src/main/java/prv/pgergely/ctscountry/webservices/CountryGtfsFeed.java
+++ cts-country/src/main/java/prv/pgergely/ctscountry/webservices/CountryGtfsFeed.java
@@ -1,5 +1,7 @@
 package prv.pgergely.ctscountry.webservices;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,23 +9,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import prv.pgergely.cts.common.domain.transitfeed.FeedLocationList;
+import prv.pgergely.cts.common.domain.mobility.GftsFeedDataList;
+import prv.pgergely.cts.common.domain.mobility.GtfsFeedData;
 import prv.pgergely.ctscountry.services.DatasourceService;
+import prv.pgergely.ctscountry.services.MobilityGtfsService;
 
 @RestController
-@RequestMapping(path="/gtfs-feed")
+@RequestMapping(path="/gtfs-content")
 public class CountryGtfsFeed {
 
 	@Autowired
 	private DatasourceService dsService;
+	
+	@Autowired
+	private MobilityGtfsService gtfsSrvc;
 	
 	@GetMapping(path="/hello")
 	public String greet() {
 		return "Test Works";
 	}
 	
-	@GetMapping(path="/feeds/{locationState}", produces = "application/json")
-	public ResponseEntity<FeedLocationList> getFeeds(@PathVariable String locationState){
-		return ResponseEntity.ok().build();
+	@GetMapping(path="/feeds/{countryCode}", produces = "application/json")
+	public ResponseEntity<GftsFeedDataList> getFeeds(@PathVariable String countryCode){
+		List<GtfsFeedData> selectedFeeds = gtfsSrvc.getFeedsByCountry(countryCode);
+		return ResponseEntity.ok(new GftsFeedDataList(selectedFeeds));
 	}
 }
